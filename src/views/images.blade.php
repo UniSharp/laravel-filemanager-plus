7a4a4d5ff62572e783b099da00ebdd885a90de08
ethanhou <yuhao1098@gmail.com>
2016-07-09 02:41:04 +0000

    fix 後台”圖片管理“最下面的地方，資料夾名稱會超出範圍 & 資料夾 跟下面的下拉選單好像有點遠

--- src/views/images.blade.php
+++ src/views/images.blade.php
@@ -5,12 +5,12 @@
 
     @foreach($directories as $key => $dir_name)
     <div class="col-sm-3 col-md-2">
-      <div class="thumbnail text-center" data-id="{{ $dir_name['long'] }}">
+      <div class="thumbnail text-center" data-id="{{ $dir_name['long'] }}" style="margin-bottom: 0px">
         <a data-id="{{ $dir_name['long'] }}" class="folder-icon pointer folder-item">
           <img src="/vendor/laravel-filemanager/img/folder.png">
         </a>
       </div>
-      <div class="caption text-center">
+      <div class="caption text-center" style="margin-bottom: 20px">
         <div class="btn-group">
           <button type="button" data-id="{{ $dir_name['long'] }}" class="btn btn-default btn-xs folder-item">
             {{ str_limit($dir_name['short'], $limit = 10, $end = '...') }}
@@ -36,11 +36,11 @@
 
     <div class="col-sm-3 col-md-2 img-row">
 
-      <div class="thumbnail thumbnail-img" data-id="{{ $file_name }}" id="img_thumbnail_{{ $key }}">
+      <div class="thumbnail thumbnail-img" data-id="{{ $file_name }}" id="img_thumbnail_{{ $key }}" style="margin-bottom: 0px">
         <img id="{{ $file }}" src="{{ $thumb_src }}" alt="" class="pointer" onclick="useFile('{{ $file_name }}')">
       </div>
 
-      <div class="caption text-center">
+      <div class="caption text-center" style="margin-bottom: 20px">
         <div class="btn-group ">
           <button type="button" onclick="useFile('{{ $file_name }}')" class="btn btn-default btn-xs">
             {{--{{ str_limit($file_name, $limit = 10, $end = '...') }}--}}
